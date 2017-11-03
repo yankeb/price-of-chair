771d7824e793f6860b10229313f34ac85921829a
Ian Kalanda <iankay14@gmail.com>
2017-11-03 10:24:20 +0300

    Adding ability to read mongo db Enviroment variable

--- src/common/database.py
+++ src/common/database.py
@@ -1,10 +1,11 @@
+import os
 import pymongo
 
 __author__ = 'jslvtr'
 
 
 class Database(object):
-    URI = "mongodb://127.0.0.1:27017"
+    URI = os.environ.get("MONGOLAB_URI")
     DATABASE = None
 
     @staticmethod
